8598e432309befda7104f50b48e0bab157f8d64a
Rakhat Alpysbai <rakhatdev@gmail.com>
2025-04-10 20:57:59 +0500

    refactor currencyRatesList widget

--- src/pages/MainPage/ui/mainPage.vue
+++ src/pages/MainPage/ui/mainPage.vue
@@ -1,11 +1,27 @@
 <template>
     <section class="mt-4 mb-4">
         <div class="container">
-            <currency-rates-list />
+            <currency-rates-list
+                :rates="filteredRates"
+                :loading="currencyRatesStore.loading"
+                @refresh="currencyRatesStore.fetchCurrencyRates"
+            />
         </div>
     </section>
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import { CurrencyRatesList } from '@/widgets/currencyRatesList'
+import { useFilteredRates } from '@/features/showCurrencyRates'
+import { useCurrencyRatesStore } from '@/entities/currencyRates'
+
+const currencyRatesStore = useCurrencyRatesStore()
+const filteredRates = useFilteredRates()
+
+onMounted(() => {
+    if (currencyRatesStore.currencyRates.length === 0) {
+        currencyRatesStore.fetchCurrencyRates()
+    }
+})
 </script>
--- src/widgets/currencyRatesList/ui/currencyRatesList.vue
+++ src/widgets/currencyRatesList/ui/currencyRatesList.vue
@@ -3,29 +3,23 @@
         <template #title>
             <div class="flex justify-content-between align-items-center">
                 <h3>Курсы валют</h3>
-                <Button
-                    icon="pi pi-refresh"
-                    @click="currencyRatesStore.fetchCurrencyRates"
-                />
+                <Button icon="pi pi-refresh" @click="emit('refresh')" />
             </div>
         </template>
         <template #content>
-            <div
-                v-if="currencyRatesStore.loading"
-                class="flex justify-content-center"
-            >
+            <div v-if="loading" class="flex justify-content-center">
                 <ProgressSpinner
                     style="width: 40px; height: 40px"
                     stroke-width="6"
                 />
             </div>
             <ul v-else class="mt-4 mb-4">
-                <li v-for="(rate, idx) in filteredRates" :key="rate.to">
+                <li v-for="(rate, idx) in rates" :key="rate.to">
                     <span>1 {{ rate.from }} = </span>
                     <span class="font-semibold">
                         {{ formatCurrencyValue(rate.value) }} {{ rate.to }}
                     </span>
-                    <Divider v-if="idx !== filteredRates.length - 1" />
+                    <Divider v-if="idx !== rates.length - 1" />
                 </li>
             </ul>
         </template>
@@ -33,23 +27,25 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-
 import Card from 'primevue/card'
 import Divider from 'primevue/divider'
 import ProgressSpinner from 'primevue/progressspinner'
 import Button from 'primevue/button'
 
-import { useFilteredRates } from '@/features/showCurrencyRates'
-import { useCurrencyRatesStore } from '@/entities/currencyRates'
 import { formatCurrencyValue } from '@/shared/lib/formatCurrencyValue'
 
-const currencyRatesStore = useCurrencyRatesStore()
-const filteredRates = useFilteredRates()
-
-onMounted(() => {
-    currencyRatesStore.fetchCurrencyRates()
+const props = defineProps({
+    rates: {
+        type: Array,
+        required: true,
+    },
+    loading: {
+        type: Boolean,
+        required: true,
+    },
 })
+
+const emit = defineEmits(['refresh'])
 </script>
 
 <style scoped>
